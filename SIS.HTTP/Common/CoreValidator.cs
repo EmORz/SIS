66f094b7d55e0b4403a72f93892704cef247a6d0
Emo <eiv.razgrad@gmail.com>
2019-05-15 15:12:49 +0300

    initial

---
+++ SIS.HTTP/Common/CoreValidator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIS.HTTP.Common
+{
+    public class CoreValidator
+    {
+        public static void ThrowIfFull(object obj, string name)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentException(name);
+
+            }
+        }
+
+        public static void ThrowIfNullOrEmpty(string text, string name)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                //Todo something is wrong!
+                throw new ArgumentException($"{name} cannot be null or empty.", name);
+            }
+
+        }
+    }
+}
