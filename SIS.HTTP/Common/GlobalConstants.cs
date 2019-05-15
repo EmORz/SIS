2d7b10e1c9f12e505817fedc4575d38f99a3136f
Emo <eiv.razgrad@gmail.com>
2019-05-15 22:40:59 +0300

    Implement methods in HttpRequest.cs

--- SIS.HTTP/Common/GlobalConstants.cs
+++ SIS.HTTP/Common/GlobalConstants.cs
@@ -11,5 +11,8 @@ namespace SIS.HTTP.Common
         public const string HostHeaderKey = "Host";
 
         public const string HttpNewLine = "\r\n";
+
+        public const string UnsupportedHttpMethodExceptionMessage = "The HTTP method - {0} is not supported.";
+
     }
 }
--- SIS.HTTP/Request/HttpRequest.cs
+++ SIS.HTTP/Request/HttpRequest.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using SIS.HTTP.Common;
 using SIS.HTTP.Enums;
+using SIS.HTTP.Exceptions;
 using SIS.HTTP.Headers;
 using SIS.HTTP.Headers.Contarct;
 using SIS.HTTP.Request.Contracts;
@@ -34,5 +36,118 @@ namespace SIS.HTTP.Request
         public HttpRequestMethod RequestMethod { get; private set; }
 
         //todo: add method!!!
+
+        private bool IsValidRequestLine(string[] requestLineParams)
+        {
+            if (requestLineParams.Length != 3 || requestLineParams[2] != GlobalConstants.HttpOneProtocolFragment)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private bool IsValidRequestQueryString(string queryString, string[] queryParameter)
+        {
+            CoreValidator.ThrowIfNullOrEmpty(queryString, nameof(queryString));
+            return true;
+
+        }
+
+        private bool HasQueryStrng()
+        {
+            return this.Url.Split('?').Length > 1;
+        }
+
+        private IEnumerable<string> ParsePlainRequestHeaders(string[] requestLines)
+        {
+            for (int i = 1; i < requestLines.Length-1; i++)
+            {
+                if (!string.IsNullOrEmpty(requestLines[i]))
+                {
+                    yield return requestLines[i];
+                }
+
+            }
+
+        }
+
+        private void ParseRequestMethod(string[] requestLineParams)
+        {
+            var parseResult = HttpRequestMethod.TryParse(requestLineParams[0], true, out HttpRequestMethod method);
+
+            if (!parseResult)
+            {
+                throw new BadRequestException(string.Format(GlobalConstants.UnsupportedHttpMethodExceptionMessage, requestLineParams[0]));
+            }
+            this.RequestMethod = method;
+        }
+
+        private void ParseRequestUrl(string[] requestLineParameter)
+        {
+            this.Url = requestLineParameter[1];
+        }
+
+        private void ParseRequestPath()
+        {
+            this.Path = this.Url.Split('?')[0];
+        }
+
+        private void ParseRequestHeaders(string[] plainHeaders)
+        {
+            plainHeaders
+                .Select(plainHeader => plainHeader.Split(new[] { ':', ' ' }, StringSplitOptions.RemoveEmptyEntries))
+                .ToList()
+                .ForEach(queryParameterKeyValuePair => this.QueryData.Add(queryParameterKeyValuePair[0], queryParameterKeyValuePair[1])));
+        }
+
+        private void ParseRequestQueryParameters()
+        {
+            if (this.HasQueryStrng())
+            {
+                this.Url.Split('?', '#')[1]
+                    .Split('?')
+                    .Select(plainQueryParameter => plainQueryParameter.Split('='))
+                    .ToList()
+                    .ForEach(queryParameterKeyValuePair => this.FormData.Add(queryParameterKeyValuePair[0], queryParameterKeyValuePair[1]));
+            }
+
+        }
+        private void ParseRequestFormDataParameters(string requestBody)
+        {
+            if (!string.IsNullOrEmpty(requestBody))
+            {
+                requestBody
+                    .Split('&')
+                    .Select(plainQueryParameter => plainQueryParameter.Split('='))
+                    .ToList()
+                    .ForEach(queryParameterKeyValuePair => this.FormData.Add(queryParameterKeyValuePair[0], queryParameterKeyValuePair[1]));
+            }
+        }
+        private void ParseRequestParameters(string requestBody)
+        {
+            this.ParseRequestQueryParameters();
+            this.ParseRequestParameters(requestBody);
+        }
+
+        private void ParseRequest(string requestString)
+        {
+            var splitRequestString = requestString.Split(new[] { GlobalConstants.HttpNewLine }, StringSplitOptions.None);
+            var requestLineParams = splitRequestString[0].Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (!IsValidRequestLine(requestLineParams))
+            {
+                throw new BadRequestException();
+            }
+
+            this.ParseRequestMethod(requestLineParams);
+
+            this.ParseRequestUrl(requestLineParams);
+
+            this.ParseRequestPath();
+
+            this.ParseRequestHeaders(this.ParsePlainRequestHeaders(splitRequestString).ToArray());
+
+            this.ParseRequestParameters(splitRequestString[splitRequestString.Length - 1]);
+        }
     }
 }
