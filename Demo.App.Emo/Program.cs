1963646da6e825a6d7bd906527d41f37c2d0ff16
Emo <eiv.razgrad@gmail.com>
2019-05-16 16:49:06 +0300

    Last update

--- Demo.App.Emo/Program.cs
+++ Demo.App.Emo/Program.cs
@@ -1,8 +1,9 @@
 ﻿using Demo.App.Emo.Contreller;
+using SIS.HTTP.Enums;
 using SIS.WebServer;
+using SIS.WebServer.Result;
 using SIS.WebServer.Routing;
 using SIS.WebServer.Routing.Contracts;
-using System;
 
 namespace Demo.App.Emo
 {
@@ -14,6 +15,10 @@ namespace Demo.App.Emo
 
             serverRoutingTable.Add(SIS.HTTP.Enums.HttpRequestMethod.Get, "/", httpRequest => new HomeController().Home(httpRequest));
 
+            //serverRoutingTable.Add(HttpRequestMethod.Get, "/", httpRequest =>
+            //{
+            //    return new HtmlResult("<h1>Hello World!</h1>", HttpResponseStatusCode.Ok);
+            //});
 
             Server server = new Server(8000, serverRoutingTable);
             server.Run();
