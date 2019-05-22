66cb357b1125ea7cb80f448d6537841194abb11b
Emo <eiv.razgrad@gmail.com>
2019-05-22 20:44:46 +0300

    Implement last component but doesnt work, must refactoring !!!!!!!!

--- SIS_V/Demo.App/Controller/HomeController.cs
+++ SIS_V/Demo.App/Controller/HomeController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using SIS.HTTP.Cookies;
 using SIS.HTTP.Requests.Contracts;
+using SIS.HTTP.Responses;
 using SIS.HTTP.Responses.Contracts;
 
 namespace Demo.App.Controller
@@ -15,7 +17,27 @@ namespace Demo.App.Controller
         }
         public IHttpResponse Index(IHttpRequest httpRequest)
         {
+            IHttpResponse newrResponse = new HttpResponse();
+
+            HttpCookie cookie = new HttpCookie("lang", "en");
+            cookie.Delete();
+
+            newrResponse.Cookies.AddCookie(cookie);
+
+            return newrResponse;
+        }
+
+        public IHttpResponse Home(IHttpRequest httpRequest)
+        {
+            if (!this.IsLogged())
+            {
+                return this.Redirect($"/login");
+            }
+
+            this.ViewData["Username"] = this.httpRequest.Session.GetParameter("username");
             return this.View();
+
+
         }
     }
 }
